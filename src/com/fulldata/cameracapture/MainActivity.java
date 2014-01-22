6c303390b8793371338fe03b8ee48f59d9c9c882
xbw <xbw>
2014-01-22 15:12:00 +0800

    0.13

--- src/com/fulldata/cameracapture/MainActivity.java
+++ src/com/fulldata/cameracapture/MainActivity.java
@@ -1,12 +1,13 @@
 package com.fulldata.cameracapture;
 
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,125 +15,137 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+public class MainActivity extends Activity implements OnClickListener {
 
-public class MainActivity extends Activity implements OnClickListener  {
-	
 	static int port;
 	Button btn;
 	EditText edittext;
 
-    private String intToIp(int i) {     
+	private String intToIp(int i) {
 
-        return (i & 0xFF ) + "." +     
+		return (i & 0xFF) + "." +
 
-      ((i >> 8 ) & 0xFF) + "." +     
+		((i >> 8) & 0xFF) + "." +
 
-      ((i >> 16 ) & 0xFF) + "." +     
+		((i >> 16) & 0xFF) + "." +
 
-      ( i >> 24 & 0xFF) ;
+		(i >> 24 & 0xFF);
 
-   } 
-	 public String getLocalIpAddress() {     
-		 WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+	}
 
+	public String getLocalIpAddress() {
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
-	        if (!wifiManager.isWifiEnabled()) {
+		if (!wifiManager.isWifiEnabled()) {
 
-	        wifiManager.setWifiEnabled(true);  
+			wifiManager.setWifiEnabled(true);
 
-	        }
+		}
 
-	        WifiInfo wifiInfo = wifiManager.getConnectionInfo();     
+		WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
-	        int ipAddress = wifiInfo.getIpAddress(); 
+		int ipAddress = wifiInfo.getIpAddress();
 
-	        return intToIp(ipAddress); 
-	    }     
+		return intToIp(ipAddress);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, 0, 0, "Exit");
+		return super.onCreateOptionsMenu(menu);
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 0:
+			finish();
+			return true;
+		default:
+			break;
+		}
+		return false;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+		// setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 		btn = (Button) findViewById(R.id.startListen);
 		btn.setOnClickListener(this);
-		
+
 		edittext = (EditText) findViewById(R.id.listenPort);
-		
-		TextView tv = (TextView)findViewById(R.id.localIP);
-		try
-		{
+
+		TextView tv = (TextView) findViewById(R.id.localIP);
+		try {
 			String ip = getLocalIpAddress();
-			if(ip!=null)
-			{
-				
+			if (ip != null) {
+
 				tv.setText(ip);
 			}
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			;
 		}
 
-		
-		
-		if(RunService.isStart)
-		{
+		if (RunService.isStart) {
 			edittext.setEnabled(false);
 			edittext.setText(Integer.toString(port));
 			btn.setText("Stop");
 		}
 	}
-	
-	void Start()
-	{
-		Intent serviceIntent = new Intent(MainActivity.this,RunService.class);
+
+	void Start() {
+		Intent serviceIntent = new Intent(MainActivity.this, RunService.class);
 		serviceIntent.putExtra(DOWNLOAD_SERVICE, port);
 		startService(serviceIntent);
 	}
-	
-	void Stop()
-	{
-		Intent serviceIntent = new Intent(MainActivity.this,RunService.class);
+
+	void Stop() {
+		Intent serviceIntent = new Intent(MainActivity.this, RunService.class);
 		stopService(serviceIntent);
 	}
-	
-	void ClickButton()
-	{
-		if(RunService.isStart)
-		{
+
+	@Override
+	public void onBackPressed() {
+		// super.onBackPressed();
+		Intent i = new Intent(Intent.ACTION_MAIN);
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		i.addCategory(Intent.CATEGORY_HOME);
+		startActivity(i);
+	}
+
+	void ClickButton() {
+		if (RunService.isStart) {
 			btn.setText("Listen");
 			edittext.setEnabled(true);
 			Stop();
-		}
-		else
-		{	
+		} else {
 			port = Integer.parseInt(edittext.getText().toString());
-			if(0<port && port<65536)
-			{
+			if (0 < port && port < 65536) {
 				btn.setText("Stop");
 				edittext.setEnabled(false);
 				Start();
+			} else {
+				Toast.makeText(getApplicationContext(), "Listen Failed",
+						Toast.LENGTH_SHORT).show();
 			}
-			else
-			{
-				Toast.makeText(getApplicationContext(),"Listen Failed",Toast.LENGTH_SHORT).show();
-			}
-			
+
 		}
 	}
 
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		switch (arg0.getId())
-		{
+		switch (arg0.getId()) {
 		case R.id.startListen:
 			ClickButton();
 			break;
-		default: break;
+		default:
+			break;
 		}
 	}
 }
--- src/com/fulldata/cameracapture/RunService.java
+++ src/com/fulldata/cameracapture/RunService.java
@@ -1,41 +1,67 @@
 package com.fulldata.cameracapture;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
-
-public class RunService extends Service{
+public class RunService extends Service {
 	HandleConnect hc;
 	static boolean isStart;
+	 WakeLock mWakeLock = null;
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
-	 @Override  
-	 public int onStartCommand(Intent intent, int flags, int startId) {  
-		if(!RunService.isStart)
-		{
-			int port = intent.getIntExtra(DOWNLOAD_SERVICE,0);
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (!RunService.isStart) {
+			int port = intent.getIntExtra(DOWNLOAD_SERVICE, 0);
 			hc = new HandleConnect();
-			if (hc.Listening(port))
-			{
+			if (hc.Listening(port)) {
 				RunService.isStart = true;
 				hc.start();
 			}
 		}
-	    return START_REDELIVER_INTENT; 
-	 }
-	 
-	 @Override
-	 public void onDestroy(){
-		 try {
+		acquireWakeLock();
+		super.onStartCommand(intent, flags, startId);
+		return START_REDELIVER_INTENT;
+	}
+
+	@Override
+	public void onDestroy() {
+		try {
 			hc.Close();
 			hc.join();
+			releaseWakeLock();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 RunService.isStart = false;
-	 }
+		RunService.isStart = false;
+	}
+
+	private void acquireWakeLock() {
+		
+		if (null == mWakeLock) {
+			PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+			mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK
+					| PowerManager.ON_AFTER_RELEASE, "LOCK");
+			if (null != mWakeLock) {
+				mWakeLock.acquire();
+			}
+		}
+	}
+
+	// 释放设备电源锁
+	private void releaseWakeLock() {
+		if (null != mWakeLock) {
+			mWakeLock.release();
+			mWakeLock = null;
+		}
+	}
 }
